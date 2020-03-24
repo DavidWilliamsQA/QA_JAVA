d89c294e7ffa289a36df5ee6e9e6c3763974acd1
David <temiloluwaw@gmail.com>
2020-03-24 15:59:56 +0000

    Hello World Commit

---
+++ helloWorld/src/helloWorld/helloworld.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+
+		String hello = "Hello World";
+		System.out.println(hello);
+		System.out.println(printsHelloWorld("yes"));
+
+	}
+
+	public static String printsHelloWorld(String word) {
+		return word;
+	}
+
+}
