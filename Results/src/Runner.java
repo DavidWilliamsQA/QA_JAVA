78babaff387e722f5afd857785c35f2f8ee5a335
David <temiloluwaw@gmail.com>
2020-03-25 14:27:04 +0000

    updated commmit for results

--- Results/src/Runner.java
+++ Results/src/Runner.java
@@ -4,13 +4,13 @@ public class Runner {
 	public static void main(String[] args) {
 
 		double physicsResult = 120;
-		double chemistryResult = 45;
-		double biologyResult = 81;
+		double chemistryResult = 110;
+		double biologyResult = 89.5;
 
-		results result = new results();
+		Results result = new Results();
 
-		result.ExamResults(physicsResult, chemistryResult, biologyResult);
-		result.ExamPercentages(physicsResult, chemistryResult, biologyResult);
+		result.examResults(physicsResult, chemistryResult, biologyResult);
+		result.examPercentages(physicsResult, chemistryResult, biologyResult);
 
 	}
 
--- Results/src/results.java
+++ Results/src/results.java
@@ -1,10 +1,10 @@
 
-public class results {
+public class Results {
 
 	double total;
 	double percentage;
 
-	public void ExamResults(double Physics, double Chemistry, double Biology) {
+	public void examResults(double Physics, double Chemistry, double Biology) {
 
 		System.out.println("Physics: " + Physics);
 		System.out.println("Chemistry: " + Chemistry);
@@ -12,15 +12,33 @@ public class results {
 
 	}
 
-	public void ExamPercentages(double Physics, double Chemistry, double Biology) {
+	public void examPercentages(double Physics, double Chemistry, double Biology) {
 
 		total = Physics + Chemistry + Biology;
 		percentage = (total * 100) / 450;
 
-		if (percentage < 60 || Physics < 60 || Chemistry < 60 || Biology < 60) {
-			System.out.println("Failed this exam with a percentage of: " + percentage);
+		double physicsPercentage = Physics * 100 / 150;
+		double chemistryPercentage = Chemistry * 100 / 150;
+		double biologyPercentage = Biology * 100 / 150;
+		int counter = 0;
+
+		if (physicsPercentage < 60) {
+			System.out.println("Failed the Physics Exam with a percentage: " + physicsPercentage);
+			counter++;
+		}
+		if (chemistryPercentage < 60) {
+			System.out.println("Failed the Chemistry Exam with a percentage: " + chemistryPercentage);
+			counter++;
+		}
+		if (biologyPercentage < 60) {
+			System.out.println("Failed the Biology Exam with a percentage: " + biologyPercentage);
+			counter++;
+		}
+
+		if (counter >= 1) {
+			System.out.println("Failed " + counter + " exams");
 		} else {
-			System.out.println("the percentage is " + percentage);
+			System.out.println("The percentage is " + percentage);
 		}
 	}
 
