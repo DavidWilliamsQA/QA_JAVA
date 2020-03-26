4e26d41c24e3feb181246ba8bd711cc577d1dc95
David <temiloluwaw@gmail.com>
2020-03-26 13:53:08 +0000

    Updated commit

--- Conditionals/src/Runner.java
+++ Conditionals/src/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 	public static void main(String[] args) {
 		flowchart flow = new flowchart();
 		conditionalExercise exercise = new conditionalExercise();
-		flow.flow(750);
+		flow.flow(2045);
 		int test = exercise.multiplyOrAdd(3, 27, false);
 		System.out.println(test);
 
